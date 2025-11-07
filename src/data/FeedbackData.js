887c4ceebc14ce114fec9c5c7e9c38e48f33318d
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2025-11-07 14:10:24 +0000

    Add FeedbackData and initialize feedback state
    Created src/data/FeedbackData.js with sample feedback items and imported it into App.js. Initialized feedback state in App.js using useState to prepare for dynamic feedback handling.

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'We finished our first sensor sweep of the neutral zone. Yesterday I did not know how to eat gagh. And blowing into maximum warp spee',
+	},
+	{
+		id: 2,
+		rating: 10,
+		text: 'Blowing into maximum warp speed, you appeared for an instant to be in two places at once. Congratulations - you just destroyed the Enterprise',
+	},
+	{
+		id: 3,
+		rating: 10,
+		text: 'They were just sucked into space. Did you come here for something in particular or just general Riker-bashing? Mr. Worf, you do remember how to fire phasers?',
+	},
+];
+export default FeedbackData;
