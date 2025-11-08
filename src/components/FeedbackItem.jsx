1e33b254eaae520d5edfdf5b11593ca8ffab508f
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2025-11-08 20:04:19 +0000

    Add delete button to feedback items using react-icons
    Introduced a delete button with an icon (FaTimes from react-icons) to each FeedbackItem. Added a deleteFeedback handler in App and passed it down to FeedbackList and FeedbackItem components. Updated dependencies to include react-icons.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
+import { FaTimes } from 'react-icons/fa';
 function FeedbackItem({ item }) {
+	const handleClick = (id) => {};
 	return (
 		<Card>
 			<div className='num-display'>{item.rating}</div>
+			<button onClick={() => handleClick(item.id)} className='close'>
+				<FaTimes color='purple' />
+			</button>
+
 			<div className='text-display'>{item.text}</div>
 		</Card>
 	);
