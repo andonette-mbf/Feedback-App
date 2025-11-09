9e41cd44c4db4aec88fca6c38cee5bfa34f9b190
andonette-mbf <94548512+andonette-mbf@users.noreply.github.com>
2025-11-09 17:06:45 +0000

    Add Button component and improve FeedbackForm validation
    Introduced a reusable Button component with support for different versions and disabled state. Updated FeedbackForm to use the new Button, added input validation to enable/disable the button and display a message if the input is too short.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 function FeedbackForm() {
 	const [text, setText] = useState('');
+	const [btnDisabled, setBtnDisabled] = useState(true);
+	const [message, setMessage] = useState('');
 	const handleTextChange = (e) => {
-		console.log(e.target.value);
+		const inputValue = e.target.value;
+		setText(inputValue);
+
+		if (inputValue.trim().length === 0) {
+			setBtnDisabled(true);
+			setMessage(null);
+		} else if (inputValue.trim().length <= 10) {
+			setBtnDisabled(true);
+			setMessage('Text must be at least 10 characters');
+		} else {
+			setMessage(null);
+			setBtnDisabled(false);
+		}
 	};
 	return (
 		<Card>
@@ -13,8 +28,11 @@ function FeedbackForm() {
 				{/*Todo - ratings select component*/}
 				<div className='input-group'>
 					<input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
-					<button type='sumbit'>Send</button>
+					<Button type='submit' isDisabled={btnDisabled}>
+						Send
+					</Button>
 				</div>
+				{message && <div className='message'>{message}</div>}
 			</form>
 		</Card>
 	);
